0e38927dd70487fccdd15acb78a2d97402ea4180
RAMAZAN HOYLADI <75537267+ramazanhoyladi@users.noreply.github.com>
2025-05-18 17:07:27 +0300

    Update vite.config.ts
    to work in all environments

--- vite.config.ts
+++ vite.config.ts
@@ -73,6 +73,10 @@ const gitInfo = getGitInfo();
 
 export default defineConfig((config) => {
   return {
+    server: {
+      host: true,
+      allowedHosts: 'all',
+    },
     define: {
       __COMMIT_HASH: JSON.stringify(gitInfo.commitHash),
       __GIT_BRANCH: JSON.stringify(gitInfo.branch),
